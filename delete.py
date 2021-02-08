134516240afdf89740aef468a53120873e0de3a5
andrew123-rev <77388784+andrew123-rev@users.noreply.github.com>
2021-02-08 14:26:12 +0600

    Update and rename .gitignore to delete.py

---
+++ delete.py
@@ -0,0 +1,28 @@
+import amino
+import aminos
+client=amino.ClientSid()
+#تسجيل
+print ("By Endru")
+
+sid=input("sid: ")
+
+client.sssid(sid=sid)
+
+#رابط المنتدى
+
+comlink=input("Community Link: ")
+
+cominfo=client.get_from_code(comlink)
+comId=cominfo.path[1:cominfo.path.index('/')]
+
+subclient=amino.SubClient(comId=comId,profile=client.profile)
+
+
+clientx=amino.acm.ACM(profile=client.profile, comId=comId)
+
+email=input("Your Email: ")
+password=input("Your Password: ")
+
+clientx.delete_community(email=email,  password=password, verificationCode=None)
+
+print ("Done")
